0609a94db3a206b9e944dcc75e346ff09e468452
Anton-Astrakhantsev <36815626+Anton-Astrakhantsev@users.noreply.github.com>
2018-03-07 04:25:37 +0300

    Update Задача.py

--- Task 2.2/Задача.py
+++ Task 2.2/Задача.py
@@ -3,23 +3,23 @@ import chardet
 origin = []
 
 
-def text(u):
+def text(u): # Функция text() разбивает фразы на список слов и добавляет в список origin
     with open(u, 'rb') as f:
         data = f.read()
         code = chardet.detect(data)
         print("Кодировка: {} с вероятностью {}\n".format(code['encoding'], code['confidence']))
     with open(u, encoding=code['encoding']) as f:
         for line in f:
-            origin.append(line.lower().strip().split(' '))
+            origin.append(line.lower().strip().split())
 
 
 te = input("Введите название файла ")
 text(te)
 
 fix = lambda l: sum(l, [])
-origin = fix(origin)
+origin = fix(origin) # "Распаковывает" списоки внутри origin, превращая его в один сплошной список
 
-seven = []
+seven = [] # В этот список будут попадать только слова длинее 6 символов
 
 
 for i in origin:
@@ -28,18 +28,18 @@ for i in origin:
 
 
 half_final = sorted(seven, key=seven.count)
-half_final.reverse()
+half_final.reverse() # Отсортированный по частоте упоминания список слов длинее 6 символов
 
 final = [half_final[0]]
 
 p = 1
-while len(final) < 10:
-    if half_final[p] != final[len(final) - 1]:
+while len(final) < 10: # Добаввлям слово в список final, если его там нет, пока не наберется 10
+    if half_final[p] not in final:
         final.append(half_final[p])
     p += 1
 
 
-def razz(o):
+def razz(o): # Функция razz() нужна только для корректного ответа
     o = str(o)
     if (o[len(o) - 1] == '2') or (o[len(o) - 1] == '3') or (o[len(o) - 1] == '4'):
         return "раза"
