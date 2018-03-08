5bf063c015efaa02addaa0185d7bb4139818a965
Anton-Astrakhantsev <36815626+Anton-Astrakhantsev@users.noreply.github.com>
2018-03-08 23:40:24 +0300

    Add files via upload

---
+++ Задача 1.py
@@ -0,0 +1,32 @@
+import os
+num = 0 # временно
+word = 0
+res_list = []
+
+# 1. собирает список первоначальных документов
+# 2. ищет среди них слово
+# 3. изменяет список файлов
+# 4. повторить 2
+
+def all_files():
+    time_list = os.listdir(path=".")
+    for i in time_list:
+        if i[-3:] = 'sql':
+            res_list.append(i)
+
+
+def peek(link): # поиск в файле
+    with open(link, encoding='utf-8') as f:
+        num = 0
+        data = f.read()
+        for line in inf:
+            num += line.count(word)
+        if num != 0: res_list.append(link)
+
+
+def find_and_point():
+    res_list = []
+    while 1:
+        word = input('Введите строку: ')
+        for i in res_list:
+            
