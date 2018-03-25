9fe4842ecbf94f6113751ef40a76b1ab54a64761
Anton-Astrakhantsev <36815626+Anton-Astrakhantsev@users.noreply.github.com>
2018-03-25 11:46:11 +0300

    Update VKAPP.py

--- Task 3.3/VKAPP.py
+++ Task 3.3/VKAPP.py
@@ -1,2 +1,17 @@
-# Программа готова на 40%
-№ Она будет содержать повышенный функционал
+# Программа готова на 60%
+# Это пробный вариант. Он возвращает список общих друзей между двумя пользователями.
+# Но Я собирался дополнить эту программу дополнительным функционалом
+
+person_f = input()
+person_s = input()
+
+params={
+    'access_token': TOKEN,
+    'v': '5.73',
+    'source_uid': person_f,
+    'target_uid': person_s
+}
+
+resp = requests.get('https://api.vk.com/method/friends.getMutual', params)
+r = resp.json()
+print(r['response'])
