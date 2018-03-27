ee8290ee9fe0d7ad24928f497558cd4dfa0330a2
Anton-Astrakhantsev <36815626+Anton-Astrakhantsev@users.noreply.github.com>
2018-03-27 13:07:41 +0300

    Update VKAPP.py

--- Task 3.3/VKAPP.py
+++ Task 3.3/VKAPP.py
@@ -1,17 +1,69 @@
-# Программа готова на 60%
-# Это пробный вариант. Он возвращает список общих друзей между двумя пользователями.
-# Но Я собирался дополнить эту программу дополнительным функционалом
+from urllib.parse import urlencode
+import requests
 
-person_f = input()
-person_s = input()
+APP_ID = 6422981
+AUTH_URL = 'https://oauth.vk.com/authorize'
 
-params={
-    'access_token': TOKEN,
-    'v': '5.73',
-    'source_uid': person_f,
-    'target_uid': person_s
+auth_data = {
+    'client_id': APP_ID,
+    'display': 'page',
+    'scope': 'friends,status',
+    'response_type': 'token',
+    'v': '5.73'
 }
 
-resp = requests.get('https://api.vk.com/method/friends.getMutual', params)
-r = resp.json()
-print(r['response'])
+# print('?'.join((AUTH_URL, urlencode(auth_data))))
+
+TOKEN = '178a9b32c608d3587ef3c7d3232f68b26b5c540381f77f7a2a3b7f94bf1387c7aab6c3ccc62510fa3a607'
+
+
+def step_first(): # Функция для ввода ID
+    id_f = input("Введите ваш ID: ")
+    id_s = input("Введите ID другого пользователя: ")
+    return [id_f, id_s]
+
+
+def step_second(x, y, t): # Функция для создания параметров
+    if x == '':
+        par = {
+            'access_token': t,
+            'v': '5.73',
+            'target_uid': y
+        }
+    else:
+        par = {
+            'access_token': t,
+            'v': '5.73',
+            'source_uid': x,
+            'target_uid': y
+        }
+    return par
+
+
+def step_third(p): # Функция для создания списка
+    resp = requests.get('https://api.vk.com/method/friends.getMutual', p)
+    return resp.json()['response']
+
+
+def step_fourth(l): # Функция для вывода списка друзей
+    print("Общие друзья:")
+    for friend in l:
+        para = {
+            'user_ids': friend,
+            'fields': 'first_name,last_name',
+            'v': '5.73'
+        }
+        fr = requests.get('https://api.vk.com/method/users.get', para).json()
+        name = " ".join((fr['response'][0]['first_name'], fr['response'][0]['last_name']))
+        link = 'https://vk.com/id' + str(friend)
+        print("{} — {}".format(name, link))
+
+
+def main():
+    step_f = step_first()
+    params = step_second(step_f[0], step_f[1], TOKEN)
+    friends_list = step_third(params)
+    step_fourth(friends_list)
+
+
+main()
